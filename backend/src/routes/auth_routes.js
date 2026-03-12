b880cef252ae075d96bd6a748fcf00d8faa5a9c8
BrunaTx <brunacpaes30@gmail.com>
2026-03-12 18:36:57 -0300

    agora, simm

--- backend/src/routes/auth_routes.js
+++ backend/src/routes/auth_routes.js
@@ -1,49 +1,40 @@
+// auth_routes.js
 const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
+// login
 router.post("/login", async (req, res) => {
-    try {
+  try {
+    const { usuario, senha } = req.body;
+    const [rows] = await db.query(
+      "SELECT id_usuario, nome FROM usuario WHERE usuario = ? AND senha = ?",
+      [usuario, senha]
+    );
 
-        const { usuario, senha } = req.body;
-
-        const [rows] = await db.query(
-            "SELECT id_usuario, nome FROM usuario WHERE usuario = ? AND senha = ?",
-            [usuario, senha]
-        );
-
-        if (rows.length === 1) {
-
-            // cria sessão
-            req.session.usuario = {
-                id: rows[0].id_usuario,
-                nome: rows[0].nome
-            };
-
-            return res.json({
-                sucesso: true,
-                nome: rows[0].nome
-            });
-
-        } else {
-
-            return res.status(401).json({
-                sucesso: false,
-                mensagem: "Usuário ou senha incorretos."
-            });
-
-        }
-
-    } catch (error) {
-
-        console.error("Erro no MySQL:", error);
-
-        return res.status(500).json({
-            sucesso: false,
-            mensagem: "Erro interno no servidor."
-        });
-
+    if (rows.length === 1) {
+      // cria sessão
+      req.session.usuario = {
+        id: rows[0].id_usuario,
+        nome: rows[0].nome
+      };
+      return res.json({ sucesso: true, nome: rows[0].nome });
+    } else {
+      return res.status(401).json({ sucesso: false, mensagem: "Usuário ou senha incorretos." });
     }
+
+  } catch (error) {
+    console.error("Erro no MySQL:", error);
+    return res.status(500).json({ sucesso: false, mensagem: "Erro interno no servidor." });
+  }
+});
+
+// logout
+router.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+    if (err) return res.status(500).send("Erro ao deslogar");
+    res.redirect("/"); // volta para a página de login
+  });
 });
 
 module.exports = router;
